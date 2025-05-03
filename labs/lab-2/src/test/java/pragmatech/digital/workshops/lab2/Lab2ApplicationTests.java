f58478dee41c2b1768ef3764e9276f4697fe85c0
Philip Riecks <mail@philipriecks.de>
2025-05-03 14:36:45 +0200

    init

---
+++ labs/lab-2/src/test/java/pragmatech/digital/workshops/lab2/Lab2ApplicationTests.java
@@ -0,0 +1,12 @@
+package pragmatech.digital.workshops.lab2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
---
+++ labs/lab-3/src/test/java/pragmatech/digital/workshops/lab3/Lab3ApplicationTests.java
@@ -0,0 +1,12 @@
+package pragmatech.digital.workshops.lab3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
